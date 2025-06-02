d307863ecd2562cb38c32c728fa6e5d2e5bb563a
Rishabh Rathore <rishabh.rathore@w3villa.com>
2025-06-02 10:13:02 +0530

    fifth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Subnav from "./components/Subnav";
-import { NavLink } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Optionbar from "./components/Optionbar";
 import Hero from "./components/Hero";
